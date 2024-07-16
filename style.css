71cbd6de1b202c5262aaa0915a70ba8174839ee9
ZohebS-dxb <zubisid@gmail.com>
2024-07-17 02:16:02 +0400

    Update style.css

--- style.css
+++ style.css
@@ -20,12 +20,13 @@ body {
 .letter-button {
     font-size: 2rem;
     font-weight: bold;
-    padding: 20px;
+    padding: 40px; /* Increase padding for larger buttons */
     background-color: green;
     color: white;
     border: none;
     border-radius: 10px;
     cursor: pointer;
+    transition: background-color 0.3s; /* Smooth transition */
 }
 
 .letter-button.red {
